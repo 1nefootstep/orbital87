fc398d5b7f998f440d5c5cdcfe3f4fa581898de2
1nefootstep <sheuxiang@gmail.com>
2019-06-27 16:05:43 +0800

    deleted filter.js
    old script code

--- simpleFilterExtension/filter.js
+++
@@ -1,6 +0,0 @@
-let badWords = "idiot,awful".split(","); // returns an array of string after splitting  
-
-
-$("body").children().each(function() {
-    $(this).html($(this).html().replace(/@/g,"$"));
-});
