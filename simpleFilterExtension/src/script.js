4c2f8cf113a921e4c9ba44e4fe9493322935aa80
1nefootstep <sheuxiang@gmail.com>
2019-07-16 20:58:35 +0800

    cleaned up and improved surrounding text blurring

--- simpleFilterExtension/src/script.js
+++ simpleFilterExtension/src/script.js
@@ -1,50 +1,96 @@
-// Code goes here
+// starting point for searching for keywords
 let rootTop = document.documentElement;
-function deepSearch(node, arr) {
-  if (node.nodeType === Node.TEXT_NODE) {
-      for (let word of arr) {
-        let regex = new RegExp(word,"ig");
-        let text = node.textContent;
-        
-        // alert(word);
-        if (text.search(regex) >= 0) {
-          console.log(text.search(regex));
-          // console.log(node.E);
-          // console.log(node.parentElement);
-          let singleParent = node.parentElement;
-        let doubleParent;
-        if (singleParent) {
-          if (singleParent.parentElement && singleParent.tagName!= 'BODY')
-            doubleParent = node.parentElement.parentElement;
-            let images = doubleParent.getElementsByTagName('IMG');
-            for(let image of images) {
-              image.classList.add("spoiler");
-            }
-        }
-        console.log("temp");
-        console.log(node);
-        console.log(singleParent);
-        console.log("Duality");
-        console.log(doubleParent);
-        console.log("cen");
-        node.parentElement.classList.add("spoiler");
-       /*  if (childOfDoubleParent != "body") {
-          childOfDoubleParent.classList.add("spoiler");
-        } */
-          node.parentElement.classList.add("spoiler");
-          break;
-        }
-        
-        // node.textContent = text.replace(regex, "***");;
+
+/*
+ * Tries to access two parents above and if possible, look for images among the children
+ * and marks them for blurring.
+ */
+function filterImage(node) {
+  let singleParent = node.parentElement;
+  let doubleParent;
+  // checks if singleParent exist
+  if (singleParent) {
+    // ensure that parent of parent exist and that it isn't body (avoid filtering everything)
+    if (singleParent.parentElement && singleParent.tagName!= 'BODY') {
+      doubleParent = node.parentElement.parentElement;
+      let images = doubleParent.getElementsByTagName('IMG');
+      for(let image of images) {
+        image.classList.add("spoiler");
       }
     }
-  if (node.firstChild) {
-    deepSearch(node.firstChild,arr);
-  }
-  if (node.nextSibling) {
-    deepSearch(node.nextSibling,arr);
   }
 }
+
+/*
+ * Returns an ancestor element that is div, span or p tag.
+ * If the node is already in html/body tag, that body/html element will be returned.
+ */
+function findSurrTag(node) {
+  let ancestor = node.parentElement;
+  let prevElement;
+  let allowedTagName = ["DIV", "SPAN", "P"];
+  let unallowedTagName = ["BODY", "HTML"];
+  let findAncestor = true;
+  while (findAncestor) {
+    // terminates if it detects body or html tag
+    for (let word of unallowedTagName) {
+      if (ancestor.tagName === word) {
+        // in the case of the node being in the body text,
+        // prevElement will be undefined so we will just return ancestor and
+        // filter the entire body/html
+        if (prevElement) {
+          return prevElement;
+        } else {
+          return ancestor;
+        }
+      }
+    }
+    for (let word of allowedTagName) {
+      if (ancestor.tagName === word) {
+        findAncestor = false;
+        break;
+      }
+    }
+    // if still need to find ancestor, assign ancestor the parent of ancestor.
+    // else return the element
+    if (findAncestor) {
+      prevElement = ancestor;
+      ancestor = ancestor.parentElement;
+    } else {
+      return ancestor;
+    }
+  }
+}
+
+/*
+ * searchAndFilter looks for text nodes within the DOM and and gives them a class that marks them for
+ * filtering
+ */
+function searchAndFilter(node, arr) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    for (let word of arr) {
+      let regex = new RegExp(word,"ig");
+      let text = node.textContent;
+      // -1 is returned when search fails to find keyword
+      if (text.search(regex) >= 0) {
+        filterImage(node);
+        let surroundingTag = findSurrTag(node);
+        surroundingTag.classList.add("spoiler");
+        break;
+      }
+    }
+  }
+  // check if there are children/siblings and perform searchAndFilter recursively
+  if (node.firstChild) {
+    searchAndFilter(node.firstChild,arr);
+  }
+  if (node.nextSibling) {
+    searchAndFilter(node.nextSibling,arr);
+  }
+}
+
+
+
 
 
 // check if the switch is on/off before filtering
@@ -61,7 +107,7 @@ let filter = function(startPoint) {
     if (typeof(item.onSwitch) === "undefined" || item.onSwitch) {
       promiseArr.then(function(item) {
         if (item.bannedWordsArr.length > 0) {
-          deepSearch(startPoint, item.bannedWordsArr);
+          searchAndFilter(startPoint, item.bannedWordsArr);
         }
       });
     }
